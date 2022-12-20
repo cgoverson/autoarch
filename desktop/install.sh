b1d18cca69fdbd70e06c236e54a02a6b97d8bcd5
cgoverson <coltoverson@gmail.com>
2022-12-20 14:02:19 -0500

    Update install.sh

--- desktop/install.sh
+++ desktop/install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# TODO: ,, add fallback image to boot options, clean up existing xfce config files, set up clean firstboot / startxfce4 / xinit/bashprofile behavior?
+# TODO: ,*GOTO LINE 105* rewrite non-efi condition to use gpt/bios install-- see https://wiki.archlinux.org/title/Partitioning#BIOS/GPT_layout_example , add fallback image to boot options, clean up existing xfce config files, set up clean firstboot / startxfce4 / xinit/bashprofile behavior?
 # Set up logging
 set -uo pipefail
 trap 's=$?; echo "$0: Error on line "$LINENO": $BASH_COMMAND"; exit $s' ERR
@@ -24,6 +24,13 @@ read -p 'Hostname for new installation: ' myhostname
 read -p 'Username for new installation: ' myusername
 read -p 'Password for new installation: ' mypassword
 
+# Detect efi mode
+if [ -d "/sys/firmware/efi/efivars" ]; then
+  efiDetect=1
+else
+  efiDetect=0
+fi
+
 # Calculate partition table
 totalRam=$(grep MemTotal /proc/meminfo | sed 's/[^0-9]*//g')
 totalRam=$(($totalRam+1000000))
@@ -31,7 +38,8 @@ totalDisk=$[ $(cat /sys/block/${device:5}/size) / 2 ]
 offset=$(($totalDisk-$totalRam))
 
 # Use fdisk manually so that it dynamically recognizes how much can be used for root dir after /boot and swap
-echo -e "g
+if ["$efiDetect" = 1]; then
+  echo -e "g
 n
 1
 
@@ -55,16 +63,46 @@ t
 w" | fdisk $device
 
 # For some reason mkfs doesn't like using variable names for devices, so temp.sh is created
-echo "mkfs.fat -F 16 ${device}1" > temp.sh
-echo "mkswap ${device}2" >> temp.sh
-echo "mkfs.ext4 ${device}3" >> temp.sh
-echo "mount ${device}3 /mnt" >> temp.sh
-echo "mkdir /mnt/boot" >> temp.sh
-echo "mount ${device}1 /mnt/boot" >> temp.sh
-echo "swapon ${device}2" >> temp.sh
-chmod 777 temp.sh
-./temp.sh
-rm temp.sh
+  echo "mkfs.fat -F 16 ${device}1" > temp.sh
+  echo "mkswap ${device}2" >> temp.sh
+  echo "mkfs.ext4 ${device}3" >> temp.sh
+  echo "mount ${device}3 /mnt" >> temp.sh
+  echo "mkdir /mnt/boot" >> temp.sh
+  echo "mount ${device}1 /mnt/boot" >> temp.sh
+  echo "swapon ${device}2" >> temp.sh
+  chmod 777 temp.sh
+  ./temp.sh
+  rm temp.sh
+else
+  echo -e "o
+n
+p
+1
+
++${totalRam}K
+t
+82
+n
+p
+2
+
+-20K
+t
+2
+83
+a
+2
+w" | fdisk $device
+
+# For some reason mkfs doesn't like using variable names for devices, so temp.sh is created
+  echo "mkswap ${device}1" >> temp.sh
+  echo "mkfs.ext4 ${device}2" >> temp.sh
+  echo "mount ${device}2 /mnt" >> temp.sh
+  echo "swapon ${device}1" >> temp.sh
+  chmod 777 temp.sh
+  ./temp.sh
+  rm temp.sh
+fi
 
 pacstrap /mnt base linux linux-firmware base-devel
 
